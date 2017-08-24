162cf9f368b8b08a04e65b90b026a9eab9ccecac
CFlohr <flohr.christiane@gmail.com>
2017-08-24 14:14:46 +0200

    Release Note und Zip-Datei mit dem Release für Iteration 1 hinzugefügt

--- TaschenrechnerConsole/Properties/AssemblyInfo.cs
+++ TaschenrechnerConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("ConsoleTaschenrechner")]
+[assembly: AssemblyTitle("TaschenrechnerConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ConsoleTaschenrechner")]
+[assembly: AssemblyCompany("AusbilderinChristianeFlohr")]
+[assembly: AssemblyProduct("TaschenrechnerConsole")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
