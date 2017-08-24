4b02ab10aace64d60cde16351e2dd6ed1aa4719c
CFlohr <flohr.christiane@gmail.com>
2017-08-24 07:22:18 +0200

    Ändert die Addieren Funktion von int auf float

--- ConsoleTaschenrechner/Program.cs
+++ ConsoleTaschenrechner/Program.cs
@@ -10,23 +10,25 @@ namespace ConsoleTaschenrechner
     {
         static void Main(string[] args)
         {
-            // User Story "Addieren": Als Anwender möchte ich 2 Zahlen eingeben und sie addieren
-            Console.WriteLine("Bitte geben Sie die 1. Zahl zwischen 0 und 10 ein: ");
+            // User Story "Addieren": Als Anwender möchte ich 2 Gleitkommazahlen zwischen 0,0 und 10,0 eingeben und sie addieren
+            Console.WriteLine("Bitte geben Sie die 1. Zahl zwischen 0,0 und 10,0 ein: ");
             string summand1 = Console.ReadLine();
-            int summandInt1 = Convert.ToInt32(summand1);
             Console.WriteLine();
 
-            Console.WriteLine("Bitte geben Sie die 2. Zahl zwischen 0 und 10 ein: ");
+            Console.WriteLine("Bitte geben Sie die 2. Zahl zwischen 0,0 und 10,0 ein: ");
             string summand2 = Console.ReadLine();
-            int summandInt2 = Convert.ToInt32(summand2);
             Console.WriteLine();
 
-            int summe = summandInt1 + summandInt2;
+            // Umwandlung der eingegebenen Variablen von Zeichenkette in Gleitkommazahl
+
+            float summandFloat1 = Convert.ToSingle(summand1);
+            float summandFloat2 = Convert.ToSingle(summand2);
+            float summe = summandFloat1 + summandFloat2;
 
             Console.WriteLine("Das ist die Summe aus {0} und {1}: {2}", summand1, summand2, summe);
 
             // die Variable summe ist überflüssig, wenn man die Ausgabe wie folgt programmiert:
-            //Console.WriteLine("Das ist die Summe aus {0} und {1}: {2}", summand1, summand2, summandInt1 + summandInt2); 
+            //Console.WriteLine("Das ist die Summe aus {0} und {1}: {2}", summand1, summand2, summandDouble1 + summandDouble2); 
 
             Console.ReadKey();
 
