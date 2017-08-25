af664185a27612bbde523de7c3cc4ce416c0f81e
CFlohr <flohr.christiane@gmail.com>
2017-08-25 16:11:36 +0200

    doppelten Code entfernt, Methoden HoleBenutzerEingabe übernimmt die Funktion das Programm zu beenden mit

--- TaschenrechnerConsole/Program.cs
+++ TaschenrechnerConsole/Program.cs
@@ -19,7 +19,33 @@ namespace TaschenrechnerConsole
         // Methode implementieren (Anweisungen in den Methodenrumpf schreiben)
         // Commit in Git
 
-        static string HoleSummanden(string ausgabeText)
+  
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Anwender möchte ich 2 Gleitkommazahlen zwischen 0,0 und 10,0 eingeben und sie addieren
+
+            string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die 1. Zahl zwischen -10,0 und 100,0 ein");
+            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die 2. Zahl zwischen -10,0 und 100,0 ein");
+
+            // Umwandlung der eingegebenen Variablen von Zeichenkette in Gleitkommazahl
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher wird
+            double ersteZahlAlsDouble = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
+
+            // Berechnung ausführen Addition
+            // die Werte in ersteZahlAlsDouble und zweiteZahlAlsDouble werden an die Methode Addieren übergeben
+            // nach Ausführen der Methode wird der ermittelte Wert an die Variable summe übergeben
+            double summe = Addiere(ersteZahlAlsDouble, zweiteZahlAlsDouble); 
+
+            // Ausgabe
+            Console.WriteLine("Das ist die Summe aus {0} und {1}: {2}", ersteZahlAlsString, zweiteZahlAlsString, summe);
+
+            // Programm beenden
+            HoleBenutzerEingabe("zum Beenden bitte <return> drücken");
+         }
+
+        // Methode zum Einlesen von Zeichenkette
+        static string HoleBenutzerEingabe(string ausgabeText)
         {
             Console.WriteLine(ausgabeText);
             string summand = Console.ReadLine();
@@ -28,39 +54,20 @@ namespace TaschenrechnerConsole
             return summand; // return muss immer die letzte Anweisung in einer Methode sein
         }
 
-        static void Main(string[] args)
-        {
-            // User Story "Addieren": Als Anwender möchte ich 2 Gleitkommazahlen zwischen 0,0 und 10,0 eingeben und sie addieren
-            string summand1 = HoleSummanden("Bitte gib die 1. Zahl zwischen -10,0 und 100,0 ein");
-            string summand2 = HoleSummanden("Bitte gib die 2. Zahl zwischen -10,0 und 100,0 ein");
-
-            // Umwandlung der eingegebenen Variablen von Zeichenkette in Gleitkommazahl
-            double summandDouble1 = Convert.ToDouble(summand1);
-            double summandDouble2 = Convert.ToDouble(summand2);
-
-            // Berechnung ausführen Addition
-            // die Werte in summandDouble1 und summandDouble2 werden an die Methode Addieren übergeben
-            // nach Ausführen der Methode wird der ermittelte Wert an die Variable summe übergeben
-            double summe = Addiere(summandDouble1, summandDouble2); 
-
-            // Ausgabe
-            Console.WriteLine("Das ist die Summe aus {0} und {1}: {2}", summand1, summand2, summe);
-
-            WarteAufBenutzereingabe();
-         }
-        // Methode zum Beenden der Consolenanwendung mit <Return>
-        static void WarteAufBenutzereingabe()
-        {
-            Console.WriteLine();
-            Console.WriteLine("zum Beenden bitte <return> drücken");
-            Console.ReadLine();
-        }
         // Methode Addiere für 2 Summanden
-        static double Addiere(double ersterSummand, double zweiterSummand) // übernimmt die Werte aus summandDouble1 und summandDouble2
+        static double Addiere(double ersterSummand, double zweiterSummand) // übernimmt die Werte aus ersteZahlAlsDouble und zweiteZahlAlsDouble
         {
             double summe = ersterSummand + zweiterSummand;  // summe ist nur innerhalb der Methode Addieren gültig, könnte auch anders heißen
 
             return summe; // summe wird als Ergebnis an summe zurückgegeben
+        }
+
+        // Methode Subtrahiere für 2 Werte
+        static double Subtrahiere(double minuend, double subtrahend)
+        {
+            double differenz = minuend - subtrahend;
+
+            return differenz;
         }
     }
 }
